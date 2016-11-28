48d58625b9d86dbb3c3f23044461d79c2f7cb55d
Sarcoma <sarcoma@live.co.uk>
2016-11-28 23:17:13 +0000

    Refactored added new classes

--- rummy.py
+++ rummy.py
@@ -14,166 +14,281 @@ class Deck:
     deck = []
     discard = []
 
-    def setDeck(self):
+    def stackDeck(self):
+        self.deck = []
+        self.discard = []
         for suit in self.suits:
             for card in self.cards:
-                self.deck.append((card, suit))
+                self.deck.append(Card(card, suit))
         shuffle(self.deck)
-
-    def deal(self, cardCount, playerCount):
-        hands = []
-        for _ in range(playerCount):
-            hands.append([])
-        for _ in range(cardCount):
-            for i in range(playerCount):
-                hands[i] += (self.deck.pop(),)
-        return tuple(hands)
 
     def checkStack(self):
         if len(self.deck) == 0:
             self.deck = self.discard
             self.discard = []
 
-    def printHand(self, hand):
+    def printDiscard(self):
+        print("Discard Pile: ", self.discard[-1].getCardColour())
+        print("..........................\n")
+
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def getCardColour(self):
+        if self.suit in [u"\u2665", u"\u2666"]:
+            return self.redCard()
+        elif self.suit in [u"\u2660", u"\u2663"]:
+            return self.blackCard()
+
+    def redCard(self):
+        return str(self.value) + "\033[1;31m" + self.suit + "\033[0m, "
+
+    def blackCard(self):
+        return str(self.value) + self.suit + ", "
+
+
+class Hand:
+    def __init__(self):
+        self.hand = []
+
+    def drawCard(self, card):
+        self.hand.append(card)
+
+    def discardCard(self, choice):
+        return self.hand.pop(choice)
+
+    def printHand(self):
         output = ''
-        for card in hand:
-            output += self.getCardColour(card)
+        for card in self.hand:
+            output += card.getCardColour()
         print(output.strip(', '))
 
-    def getCardColour(self, card):
-        if card[1] in [u"\u2665", u"\u2666"]:
-            return self.redCard(card)
-        elif card[1] in [u"\u2660", u"\u2663"]:
-            return self.blackCard(card)
-
-    @staticmethod
-    def redCard(card):
-        return str(card[0]) + "\033[1;31m" + card[1] + "\033[0m, "
-
-    @staticmethod
-    def blackCard(card):
-        return str(card[0]) + card[1] + ", "
-
-    @staticmethod
-    def printKey(hand):
+    def printKey(self):
         output = ''
-        for i in range(len(hand)):
+        for i in range(len(self.hand)):
             output += " %i, " % (i + 1)
         print(output.strip(', '))
 
-    def drawCard(self, hand):
-        hand.append(self.deck.pop())
-
-    def discardCard(self, hand, choice):
-        self.discard.append(hand.pop(choice - 1))
+    def printHandAndKey(self):
+        self.printHand()
+        self.printKey()
 
 
-class Rummy(Deck):
-    playerOneScore = 0
-    playerTwoScore = 0
-    lastTurn = False
-    knocked = False
-    turn = 1
-    thisPlayer = 1
+class Dealer:
+    cardCount = 7
 
-    def __init__(self):
-        self.setDeck()
-        self.playGame()
+    def setCardCount(self, cardCount):
+        self.cardCount = cardCount
 
-    def choosePickUp(self, hand):
-        choice = ''
-        while choice.lower() not in ['d', 'p']:
-            self.printHand(hand)
-            print("Discard Pile: ", self.getCardColour(self.discard[-1]))
-            print("..........................\n")
-            choice = input("Enter 'd' to draw or 'p' to pickup discard: ")
-        if choice == 'p':
-            hand.append(self.discard.pop())
-        else:
-            self.drawCard(hand)
+    def deal(self, deck):
+        hand = Hand()
+        for _ in range(self.cardCount):
+            hand.drawCard(deck.pop())
+        return hand
 
-    def checkKnocked(self):
-        if self.knocked:
-            self.lastTurn = True
 
-    def beginTurn(self, currentPlayer, hand):
-        print("###########################\n")
-        print("Turn %i, Player %i\n" % (self.turn, currentPlayer))
-        if self.knocked:
-            print("Player %i knocked last turn!!!\n" % ((currentPlayer + 1) % 2))
-        if len(self.discard) > 0:
-            self.choosePickUp(hand)
-        else:
-            self.drawCard(hand)
+class Player:
+    def __init__(self, num):
+        self.num = num
+        self.score = 0
+        self.hand = Hand()
 
-    def playHand(self, hand):
-        message = "Enter a number to discard a card or 'k' to Knock: "
-        choice = ""
+    def getPlayerName(self):
+        return "Player %i" % self.num
 
-        while choice not in [str(i) for i in range(1, 9)]:
-            if self.knocked:
-                message = "Enter a number to discard a card: "
-            choice = input(message)
-            if choice.lower() == "k":
-                self.knocked = True
+    def updateScore(self, points):
+        self.score += points
 
-        choice = int(choice)
-        self.discardCard(hand, choice)
+    def getScore(self):
+        return self.score
 
-    def endRound(self, playerOneHand, playerTwoHand):
-        print("\n***************************")
-        print("Round Ended, Enter Your Scores")
-        print("Player One:")
-        self.printHand(playerOneHand)
-        print("Player Two:")
-        self.printHand(playerTwoHand)
-        print("***************************")
-        self.playerOneScore += self.enterPlayerScore("Enter player one score: ")
-        self.playerTwoScore += self.enterPlayerScore("Enter player two score: ")
+    def getHand(self):
+        return self.hand
 
-    @staticmethod
-    def enterPlayerScore(message):
+    def enterPlayerScore(self):
         roundScore = False
         while type(roundScore) is not int:
             try:
-                roundScore = int(input(message))
+                roundScore = int(input("Enter %s score: " % self.getPlayerName()))
             except ValueError:
                 print("Please enter a valid score")
             else:
-                return roundScore
+                self.score += roundScore
 
-    def displayCurrentScores(self):
-        print("Player One Score:", self.playerOneScore)
-        print("Player Two Score:", self.playerTwoScore)
 
-    def endGame(self):
-        winner = "Player One" if self.playerOneScore < self.playerTwoScore else "Player Two"
-        print(winner, "is the Winner!!")
-        self.displayCurrentScores()
+class Round(Dealer, Deck):
+    firstPlayer = 0
+    currentPlayer = 0
+    turn = 1
+    lastTurn = 1
+    knocked = False
+
+    def __init__(self, numberOfPlayers):
+        self.numberOfPlayers = numberOfPlayers
+        self.stackDeck()
+
+    def prepareNewRound(self):
+        self.rotateFirstPlayer()
+        self.turn = 1
+        self.lastTurn = 1
+        self.knocked = False
+        self.stackDeck()
+
+    def dealCards(self, players):
+        for p in players:
+            p.hand = self.deal(self.deck)
+
+    def prepareTurn(self):
+        self.checkStack()
+        self.checkKnocked()
+
+    def endTurn(self):
+        self.switchCurrentPlayer()
+        self.turn += 1
+
+    def checkKnocked(self):
+        if self.knocked:
+            self.lastTurn += 1
+
+    def switchCurrentPlayer(self):
+        self.currentPlayer = (self.currentPlayer + 1) % self.numberOfPlayers
+
+    def rotateFirstPlayer(self):
+        self.firstPlayer += 1
+        self.currentPlayer = self.firstPlayer % self.numberOfPlayers
+
+
+class Rummy:
+    players = []
+
+    def __init__(self, numberOfPlayers):
+        self.numberOfPlayers = numberOfPlayers
+        self.createPlayers()
+        self.round = Round(numberOfPlayers)
+        self.round.dealCards(self.players)
+        self.playGame()
+
+    def createPlayers(self):
+        self.players = [Player(i + 1) for i in range(self.numberOfPlayers)]
 
     def playGame(self):
-        (playerOneHand, playerTwoHand) = self.deal(7, 2)
-        currentPlayer = 1
-        while not self.lastTurn:
-            self.checkStack()
-            self.checkKnocked()
-            hand = playerOneHand if currentPlayer == 1 else playerTwoHand
-            self.beginTurn(currentPlayer, hand)
-            self.printHand(hand)
-            self.printKey(hand)
-            self.playHand(hand)
-            currentPlayer = (currentPlayer + 1) % 2
-            self.turn += 1
-        self.endRound(playerOneHand, playerTwoHand)
-        if self.playerOneScore < 100 and self.playerTwoScore < 100:
-            self.displayCurrentScores()
-            self.lastTurn = False
-            self.knocked = False
-            self.turn = 1
-            self.setDeck()
-            self.playGame()
+        while self.round.lastTurn != self.numberOfPlayers:
+            self.round.prepareTurn()
+            hand = self.getCurrentPlayersHand()
+            self.playTurn(hand)
+            self.round.endTurn()
+        self.endRound()
+        self.startNewRoundOrEndGame()
+
+    def playTurn(self, hand):
+        self.printPlayersTurn()
+        self.playerChooseToDiscardOrPickUp(hand)
+        hand.printHandAndKey()
+        self.playerDiscardOrKnock(hand)
+
+    def getCurrentPlayersHand(self):
+        hand = self.players[self.round.currentPlayer].getHand()
+        return hand
+
+    def printPlayersTurn(self):
+        print("###########################\n")
+        print("Turn %i, %s\n" % (self.round.turn, self.getCurrentPlayerName()))
+
+    def playerChooseToDiscardOrPickUp(self, hand):
+        if self.round.knocked:
+            print("Player %i knocked, this is your last turn!!!\n" % ((self.round.currentPlayer + 1) % 2))
+        if len(self.round.discard) > 0:
+            self.choosePickUp(hand)
         else:
+            hand.drawCard(self.round.deck.pop())
+
+    def playerDiscardOrKnock(self, hand):
+        message = "Enter a number to discard a card or 'k' to Knock: "
+        choice = ""
+        while choice not in [str(i) for i in range(1, 9)]:
+            if self.round.knocked:
+                message = "Enter a number to discard a card: "
+            choice = input(message)
+            if choice.lower() == "k":
+                self.round.knocked = True
+        choice = int(choice) - 1
+        self.round.discard.append(hand.discardCard(choice))
+
+    def choosePickUp(self, hand):
+        choice = self.getUserPickUpInput(hand)
+        if choice == 'p':
+            hand.drawCard(self.round.discard.pop())
+        else:
+            hand.drawCard(self.round.deck.pop())
+
+    def getUserPickUpInput(self, hand):
+        choice = ''
+        while choice.lower() not in ['d', 'p']:
+            hand.printHand()
+            self.round.printDiscard()
+            choice = input("Enter 'd' to draw or 'p' to pickup discard: ")
+        return choice
+
+    def getCurrentPlayerName(self):
+        return self.players[self.round.currentPlayer].getPlayerName()
+
+    def startNewRoundOrEndGame(self):
+        if self.isEndOfGame():
             self.endGame()
+        else:
+            self.displayCurrentScores()
+            self.round.prepareNewRound()
+            self.playGame()
+
+    def endRound(self):
+        print("\n***************************")
+        print("Round Ended, Enter Your Scores")
+        self.printAllPlayersHands()
+        print("***************************")
+        self.enterAllPlayersScores()
+
+    def printAllPlayersHands(self):
+        for p in self.players:
+            print("%s:" % p.getPlayerName())
+            p.hand.printHand()
+
+    def enterAllPlayersScores(self):
+        for p in self.players:
+            p.enterPlayerScore()
+
+    def displayCurrentScores(self):
+        for p in self.players:
+            print(p.getPlayerName(), ": ", p.getScore())
+
+    def isEndOfGame(self):
+        for p in self.players:
+            if p.getScore() > 100:
+                return True
+        return False
+
+    def endGame(self):
+        winners = self.findLowestScores()
+        if len(winners) == 1:
+            print(winners[0], "is the Winner!!")
+        else:
+            print(", ".join([w.getPlayerName() for w in winners]), "are joint winners!")
+        self.displayCurrentScores()
+
+    def findLowestScores(self):
+        lowest = []
+        for p in self.players:
+            if not lowest:
+                lowest = [p]
+                continue
+            if p.getScore() < lowest[0].getScore():
+                lowest = [p]
+            elif p.getScore() == lowest[0].getScore():
+                lowest.append(p)
+        return lowest
+
 
 # start game
-Rummy()
+Rummy(3)
